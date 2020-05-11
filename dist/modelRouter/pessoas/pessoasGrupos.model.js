873f50d84d146779822ed381205bbd51688f21cd
asgvitor <asgvitor@gmail.com>
2020-05-11 11:27:49 -0300

    CorrecaoModeloPessoaGrupo

--- dist/modelRouter/pessoas/pessoasGrupos.model.js
+++ dist/modelRouter/pessoas/pessoasGrupos.model.js
@@ -7,32 +7,32 @@ class PessoaGrupo extends sequelize.Model {
 exports.PessoaGrupo = PessoaGrupo;
 PessoaGrupo.init({
     PessoaId: {
-        field: 't001_id_pessoa',
+        field: 'pes_id_pessoa',
         type: sequelize.DataTypes.BIGINT,
         allowNull: false,
         primaryKey: true,
         autoIncrement: false,
     },
     GrupoId: {
-        field: 't002_id_grupo',
+        field: 'grp_id_grupo',
         type: new sequelize.DataTypes.STRING(255),
         allowNull: false,
         primaryKey: true,
         autoIncrement: false,
     },
     data_inclusao: {
-        field: 't003_dt_inclusao',
+        field: 'pgp_dt_inclusao',
         type: new sequelize.DataTypes.DATE,
         allowNull: false
     },
     data_remocao: {
-        field: 't003_dt_remocao',
+        field: 'pgp_dt_remocao',
         type: new sequelize.DataTypes.DATE,
         allowNull: true
     }
 }, {
     schema: 'erp_paroquia',
-    tableName: 't003_pessoa_grupo',
+    tableName: 'tb_pgp_pessoa_grupo',
     sequelize: conexao_db_1.database,
     timestamps: false
 });
